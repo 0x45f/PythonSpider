a4c5d33a471a6503967166fb8be3e3d69eade2fc
wz <516310189@qq.com>
2017-09-27 22:58:52 +0800

    update

--- AiWenSpider/AiWen.py
+++ AiWenSpider/AiWen.py
@@ -1,19 +1,45 @@
 import requests
+from lxml import html
+
 
 class AiWenSpider:
+
 	def __init__(self):
+		# 要抓取多少页
+		self.__pages = 10
+		# 当前页数
+		self.__currentPage = 1
 		# 持久会话
 		self.__session = requests.Session()
 		# 使用代理
 		self.__session.proxies.update({'https': '171.39.29.221'})
 		# http://iask.sina.com.cn/c/95-all-1-new.html
 		# iask.sina.com.cn作为基础地址，/c/95-all-1-new.html部分是每一页问题的详细地址
-		self.__baseURL = 'iask.sina.com.cn'
+		self.__baseURL = 'http://iask.sina.com.cn'
+		self.__nextFootURL = '/c/95-all-1-new.html'
 
-	def getHTML(self, footURl):
-		response = self.__session.get(self.__baseURL + footURl)
-		return response.text
+	def getTree(self, NextFootURl):
+		response = self.__session.get(self.__baseURL + NextFootURl, timeout=5)
+		return html.fromstring(response.text)
 
+	def getNextFootURL(self, tree):
+		result = tree.xpath('//div[@class="page mt30"]/a[@class="current"]/following::a[2]')
+		return result[0].get('href')
+
+	def getQusFootURL(self, tree):
+		result = tree.xpath('//div[@class="question-title"]')
+		list = []
+		for i in result:
+			list.append(i[i].get('href'))
+		return list
+
+
+	def start(self):
+		for self.__currentPage in range(self.__pages):
+			tree = self.getTree(self.__nextFootURL)
+			# 找到下一页的url
+			self.__nextFootURL = self.getNextFootURL(tree)
 
 if __name__ == '__main__':
 	spider = AiWenSpider()
+	spider.start()
--- AiWenSpider/README.md
+++ AiWenSpider/README.md
@@ -1,4 +1,62 @@
 遍历所有的问题，来抓取每一个详情页面，提取问题，问题内容，回答者，回答时间，回答内容。
 
+
+XPath
+
 /绝对路径？
 //表示选取所有？
+
+
+Element属性方法
+
+　　tag：string，元素代表的数据种类。
+　　text：string，元素的内容。
+　　tail：string，元素的尾形。
+　　attrib：dictionary，元素的属性字典。
+　　
+　　＃针对属性的操作
+　　clear()：清空元素的后代、属性、text和tail也设置为None。
+　　get(key, default=None)：获取key对应的属性值，如该属性不存在则返回default值。
+　　items()：根据属性字典返回一个列表，列表元素为(key, value）。
+　　keys()：返回包含所有元素属性键的列表。
+　　set(key, value)：设置新的属性键与值。
+
+
+　　＃针对后代的操作
+　　append(subelement)：添加直系子元素。
+　　extend(subelements)：增加一串元素对象作为子元素。＃python2.7新特性
+　　find(match)：寻找第一个匹配子元素，匹配对象可以为tag或path。
+　　findall(match)：寻找所有匹配子元素，匹配对象可以为tag或path。
+　　findtext(match)：寻找第一个匹配子元素，返回其text值。匹配对象可以为tag或path。
+　　insert(index, element)：在指定位置插入子元素。
+　　iter(tag=None)：生成遍历当前元素所有后代或者给定tag的后代的迭代器。＃python2.7新特性
+　　iterfind(match)：根据tag或path查找所有的后代。
+　　itertext()：遍历所有后代并返回text值。
+　　remove(subelement)：删除子元素。
+
+
+XPath 轴（Axes）
+
+ancestor	   选取当前节点的所有先辈（父、祖父等）。
+
+ancestor-or-self	选取当前节点的所有先辈（父、祖父等）以及当前节点本身。
+
+attribute	选取当前节点的所有属性。
+
+child	选取当前节点的所有子元素。
+
+descendant	选取当前节点的所有后代元素（子、孙等）。
+
+descendant-or-self	选取当前节点的所有后代元素（子、孙等）以及当前节点本身。
+
+following	选取文档中当前节点的结束标签之后的所有节点。
+
+namespace	选取当前节点的所有命名空间节点。
+
+parent	选取当前节点的父节点。
+
+preceding	选取文档中当前节点的开始标签之前的所有节点。
+
+preceding-sibling	选取当前节点之前的所有同级节点。
+
+self	选取当前节点。
